ac01273252f78f7c1666448add9231694f4822e7
Kyle Decot <kyle@joinroot.com>
2019-01-11 14:57:19 -0500

    Adds transformer for eye color

---
+++ src/transformers/__test__/eye-color.test.js
@@ -0,0 +1,15 @@
+import eyeColorTransformer from '../eye-color';
+
+describe('eyeColorTransformer', () => {
+  describe('when eye color is supported', () => {
+    it('transforms correctly', () => {
+      expect(eyeColorTransformer('Hazel')).toEqual('HAZ')
+    })
+  })
+
+  describe('when eye color is not supported', () => {
+    test('throws an error', () => {
+      expect(() => eyeColorTransformer('Clear')).toThrowError('Unsupported Eye Color: Clear')
+    })
+  })
+});
---
+++ src/transformers/eye-color.js
@@ -0,0 +1,26 @@
+export default (value) => {
+  switch (value) {
+    case 'Black':
+      return 'BLK'
+    case 'Blue':
+      return 'BLU'
+    case 'Brown':
+      return 'BRO'
+    case 'Dichromatic':
+      return 'DIC'
+    case 'Gray':
+      return 'GRY'
+    case 'Green':
+      return 'GRN'
+    case 'Hazel':
+      return 'HAZ'
+    case 'Maroon':
+      return 'MAR'
+    case 'Pink':
+      return 'PNK'
+    case 'Unknown':
+      return 'UNK'
+    default:
+      throw new Error(`Unsupported Eye Color: ${value}`)
+  }
+}
